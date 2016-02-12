56c3556a7080f42d4c6103fcc3cbfef555691119
wittawasw💤 <start@wittawasw.com>
2016-02-12 11:40:35 +0700

    add fixtures, presentations ctrl spec

--- spec/controllers/presentations_controller_spec.rb
+++ spec/controllers/presentations_controller_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe PresentationsController, type: :controller do
+  fixtures :presentations
+  describe '#show' do
+    let(:memory_game) { presentations(:memory_game) }
+
+    before            { get :show, id: memory_game.id }
+
+    it { expect(response).to render_template('presentations/show') }
+  end
 end
