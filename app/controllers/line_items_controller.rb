46ec1593270e190a935a6c6471f075db084b2dc0
wittawasw💤 <start@wittawasw.com>
2016-03-08 19:05:20 +0700

    fixed line_items#create

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,7 @@
 class LineItemsController < ApplicationController
   def create
     @order = Order.find(params[:order_id])
-    line_item = LineItem.first_or_create(line_item_params)
+    line_item = LineItem.where(line_item_params).first_or_create
     line_item.update_attributes(quantity: line_item.quantity + 1)
     redirect_to @order
   end
--- spec/controllers/line_items_controller_spec.rb
+++ spec/controllers/line_items_controller_spec.rb
@@ -2,10 +2,65 @@ require 'rails_helper'
 
 RSpec.describe LineItemsController, type: :controller do
   fixtures :line_items
+  fixtures :products
   fixtures :orders
 
-  let(:buy_cards) { orders(:buy_cards) }
+  let(:buy_cards)       { orders(:buy_cards) }
+  let(:line_item_cards) { line_items(:line_item_cards) }
+  let(:number_cards)    { products(:number_cards) }
+  let(:number_board)    { products(:number_board) }
 
   describe '#create' do
+    context 'create new line_item' do
+      it 'increase line_item by 1' do
+        expect {
+          post :create,
+               order_id: buy_cards.id,
+               line_item: {
+                product_id: number_board.id,
+               }
+        }.to change{ LineItem.count }.by(1)
+      end
+
+      context '#attributes' do
+        before do
+          post :create,
+               order_id: buy_cards.id,
+               line_item: {
+                product_id: number_board.id,
+               }
+        end
+
+        it 'have quantity = 1' do
+          expect(LineItem.last.quantity).to eq(1)
+        end
+      end
+    end
+
+    context 'existing line_item with same product' do
+      it 'increase line_item by 0' do
+        expect {
+          post :create,
+               order_id: buy_cards.id,
+               line_item: {
+                product_id: number_cards.id,
+               }
+        }.to change{ LineItem.count }.by(0)
+      end
+
+      context '#attributes' do
+        before do
+          post :create,
+               order_id: buy_cards.id,
+               line_item: {
+                product_id: number_cards.id,
+               }
+        end
+
+        it 'increase quantity by 1' do
+          expect(line_item_cards.quantity).to eq(3)
+        end
+      end
+    end
   end
 end
