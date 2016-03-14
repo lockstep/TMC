d4f666bb2149eba864f4531146e7d771fa4bcac4
wittawasw💤 <start@wittawasw.com>
2016-03-14 16:55:02 +0700

    display sign_in link if not siged_in on checkout
    - checkout helper's seperated into actions and btn

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,7 @@ RSpec.configure do |config|
 
   config.include(Devise::TestHelpers, type: :controller)
   config.include(Devise::TestHelpers, type: :view)
+  config.include(Devise::TestHelpers, type: :helper)
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
