494e3adb14b6dfcf16900331afc7d427aaeee5bd
wittawasw💤 <start@wittawasw.com>
2016-03-21 18:10:13 +0700

    confirmed emails after payment
    - confirmed order email
    - confirmed charge email

---
+++ spec/mailers/orders_mailer_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe OrdersMailer, type: :mailer do
+  include_context 'before_after_mailer'
+  fixtures :users
+  fixtures :orders
+  fixtures :charges
+  fixtures :line_items
+
+  let(:michelle)      { users(:michelle) }
+  let(:cards_order)   { orders(:cards_order) }
+  let(:cards_charge)  { charges(:cards_charge) }
+
+  describe '.confirmed_order' do
+    before { OrdersMailer.confirmed_order(cards_order.id).deliver_now }
+
+    it_behaves_like "sending_email" do
+      let(:sender)     { ['noreply@tmc.com'] }
+      let(:recipients) { [ michelle.email] }
+      let(:subject)    { "Confirmed Order##{cards_order.id}" }
+    end
+  end
+end
