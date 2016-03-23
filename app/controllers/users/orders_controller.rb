3c0141694f1137cf374f0bba0d34e0effbd4277a
wittawasw💤 <start@wittawasw.com>
2016-03-23 17:50:26 +0700

    fixed order ability

--- app/controllers/users/orders_controller.rb
+++ app/controllers/users/orders_controller.rb
@@ -1,17 +1,21 @@
 class Users::OrdersController < ApplicationController
   before_action :set_order, only: [:show]
   before_action :set_user, only: [:show, :index]
+  before_action :authorize_order, only: [:show]
 
   def index
-    @orders = @user.orders.paids
     authorize! :show, @user
+    @orders = @user.orders.paids
   end
 
   def show
-    authorize! :show, (@user || @order)
   end
 
   private
+
+  def authorize_order
+    authorize! :show, (@user && @order)
+  end
 
   def set_order
     @order = Order.includes(line_items: :product)
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,6 @@ class Ability
   def initialize(user)
     user ||= User.new
     can [:show, :update], User, id: user.id
-    can :show, Order
+    can :show, Order, user_id: user.id
   end
 end
