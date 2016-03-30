555cb260f161fd7e57af8e6d94ae3c6d82936bc9
wittawasw💤 <start@wittawasw.com>
2016-03-30 11:00:25 +0700

    remove materials tables

--- db/schema.rb
+++ db/schema.rb
@@ -76,18 +76,6 @@ ActiveRecord::Schema.define(version: 20160329083514) do
   add_index "line_items", ["order_id"], name: "index_line_items_on_order_id", using: :btree
   add_index "line_items", ["product_id"], name: "index_line_items_on_product_id", using: :btree
 
-  create_table "materials", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "materials_presentations", id: false, force: :cascade do |t|
-    t.integer "presentation_id", null: false
-    t.integer "material_id",     null: false
-  end
-
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
