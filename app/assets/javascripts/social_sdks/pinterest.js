3fb6ac21f9f6db0e12dd646464dcf320ee44fa42
Jan Klimo <jan.klimo@gmail.com>
2016-04-25 11:53:46 +0700

    fix assets precompile error

--- app/assets/javascripts/social_sdks/pinterest.js
+++ app/assets/javascripts/social_sdks/pinterest.js
@@ -3,9 +3,6 @@ var Pinterest;
 Pinterest = {
   load: function() {
     delete window["PIN_" + ~~((new Date).getTime() / 864e5)];
-    $('head').append(`<script async defer
-                              data-pin-hover="true"
-                              src="//assets.pinterest.com/js/pinit.js">
-                      </script>`);
+    $('head').append("<script async defer data-pin-hover='true' src='//assets.pinterest.com/js/pinit.js'></script>");
   }
 };
