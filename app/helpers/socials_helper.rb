291def16b8d58af7d0df21eca84c8f04beb34fc3
Jan Klimo <jan.klimo@gmail.com>
2016-06-06 11:12:20 +0800

    fix case

--- app/helpers/socials_helper.rb
+++ app/helpers/socials_helper.rb
@@ -1,7 +1,7 @@
 module SocialsHelper
   DEFAULT_IMAGE = 'https://s3.amazonaws.com/lockstep-public-assets/tmc/tmc-primary-logo.png'
   DEFAULT_AUTHOR = 'The Montessori Company'
-  DEFAULT_DESCRIPTION = 'Beautiful Montessori resources, Lovingly crafted
+  DEFAULT_DESCRIPTION = 'Beautiful Montessori resources, lovingly crafted
   by an AMI Montessori guide.'
   FB_PAGE = 'fb_page_here'
   TWITTER_ACCOUNT = '@twitter_account'
