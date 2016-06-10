5468fbe2a6e9afe815030116c0723a5e29e94591
Jan Klimo <jan.klimo@gmail.com>
2016-06-10 15:29:17 +0700

    secure Elasticsearch

--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -1,9 +1,7 @@
 require 'typhoeus/adapters/faraday'
 
 Searchkick.client = Elasticsearch::Client.new(
-  hosts: [
-    ENV['ELASTICSEARCH_URL'] || 'localhost:9200'
-  ],
+  url: ENV['ELASTICSEARCH_URL'] || 'http://localhost:9200',
   retry_on_failure: true,
 )
 
