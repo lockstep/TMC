e66a14b0fb6152a7d2bb2665ccbcc2f8f94140e4
Paul Hiatt <hiattp@gmail.com>
2016-06-14 17:37:15 +0700

    fixed email typo

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   include Devise::Controllers::UrlHelpers
   default from: 'The Montessori Company <support@themontessoricompany.com>',
-    reply_to: 'Michelle <michelle@themontessoricompany.com'
+    reply_to: 'Michelle <michelle@themontessoricompany.com>'
   layout 'mailer'
 end
