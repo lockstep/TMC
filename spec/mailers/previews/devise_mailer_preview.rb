ce63460128058d125f511ec4675b85c26caf1d68
Jan Klimo <jan.klimo@gmail.com>
2016-06-15 17:37:34 +0700

    fix styles of password reset and related mailers

--- spec/mailers/previews/devise_mailer_preview.rb
+++ spec/mailers/previews/devise_mailer_preview.rb
@@ -2,4 +2,12 @@ class DeviseMailerPreview < ActionMailer::Preview
   def confirmation_instructions
     Devise::Mailer.confirmation_instructions(User.last, 12345678)
   end
+
+  def reset_password_instructions
+    Devise::Mailer.reset_password_instructions(User.last, 12345678)
+  end
+
+  def password_change
+    Devise::Mailer.password_change(User.last)
+  end
 end
