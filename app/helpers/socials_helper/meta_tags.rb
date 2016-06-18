06b6ed67487d9a75c074bd084b9fe32a3d58e7a6
Jan Klimo <jan.klimo@gmail.com>
2016-06-18 20:01:44 +0200

    fix share dialog image first load

--- app/helpers/socials_helper/meta_tags.rb
+++ app/helpers/socials_helper/meta_tags.rb
@@ -33,7 +33,10 @@ module SocialsHelper::MetaTags
     <<-META
       <meta property='og:title' content='#{title}'/>
       <meta property='og:type' content='#{type}'/>
+
       <meta property='og:image' content='#{image}'/>
+      <meta property="og:image:width" content="400"/>
+      <meta property="og:image:height" content="400"/>
       <meta property='og:url' content='#{url}'/>
 
       <meta property='og:site_name' content='#{t(:site_name)}'/>
