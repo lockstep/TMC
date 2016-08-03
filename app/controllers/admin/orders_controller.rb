bf9485e4dbab7140191c1d62c7150af6769559bf
Jan Klimo <jan.klimo@gmail.com>
2016-08-04 00:53:55 +0700

    admin search by id

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,3 +2,25 @@ module Admin
   class OrdersController < Admin::ApplicationController
   end
 end
+
+module Administrate
+  class Search
+    def initialize(resolver, term)
+      @resolver = resolver
+      @term = term
+    end
+
+    def run
+      if @term.blank?
+        resource_class.all
+      else
+        resource_class.where(id: @term)
+      end
+    end
+
+    private
+
+    delegate :resource_class, to: :resolver
+    attr_reader :resolver, :term
+  end
+end
