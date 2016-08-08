4c36ac06801d40b4202348db96f61190c6a692d9
Jan Klimo <jan.klimo@gmail.com>
2016-08-08 23:39:13 +0700

    best-sellers in welcome email

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -1,6 +1,7 @@
 class UsersMailer < ApplicationMailer
   def welcome_new_user(user_id)
     @user = User.find(user_id)
+    @results = Product.search('*', order: { times_sold: :desc }).take(4)
     mail(to: @user.email,
          subject: "#{I18n.t('emails.welcome')} #{I18n.t('site_name')}"
         )
--- spec/mailers/users_mailer_spec.rb
+++ spec/mailers/users_mailer_spec.rb
@@ -1,10 +1,14 @@
-require "rails_helper"
-
-RSpec.describe UsersMailer, type: :mailer do
+describe UsersMailer, type: :mailer do
   include_context 'before_after_mailer'
+
   fixtures :users
+  fixtures :products
+  fixtures :line_items
+  fixtures :orders
 
   let(:michelle)  { users(:michelle) }
+
+  before { Product.reindex }
 
   describe '.welcome_new_user' do
     before { UsersMailer.welcome_new_user(michelle.id).deliver_now }
@@ -12,6 +16,13 @@ RSpec.describe UsersMailer, type: :mailer do
     it_behaves_like "sending_email" do
       let(:recipients) { [ michelle.email] }
       let(:subject)    { 'Welcome to The Montessori Company' }
+    end
+
+    it 'renders best-sellers correctly' do
+      flamingo = products(:flamingo)
+      email = ActionMailer::Base.deliveries.last.encoded
+      expect(email).to match flamingo.name
+      expect(email).to match product_url(flamingo)
     end
   end
 end
