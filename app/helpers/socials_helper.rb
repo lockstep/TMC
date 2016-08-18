f64e4922306e0026d3867e033a43434453dfe599
Jan Klimo <jan.klimo@gmail.com>
2016-08-18 10:45:16 +0700

    optimize for montessori materials

--- app/helpers/socials_helper.rb
+++ app/helpers/socials_helper.rb
@@ -1,8 +1,9 @@
 module SocialsHelper
   DEFAULT_IMAGE = 'https://s3.amazonaws.com/lockstep-public-assets/tmc/tmc-primary-logo.png'
   DEFAULT_AUTHOR = 'The Montessori Company'
-  DEFAULT_DESCRIPTION = 'Beautiful Montessori resources, lovingly crafted
-  by an AMI Montessori guide.'
+  DEFAULT_DESCRIPTION = "Montessori materials lovingly hand-crafted by an " \
+    "AMI Montessori guide. Our Montessori materials are perfect for " \
+    "primary or home-school environments."
   TWITTER_ACCOUNT = '@themontessorico'
   FB_PAGE = 'https://www.facebook.com/themontessoricompany/'
 end
--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -3,9 +3,7 @@ class UsersMailer < ApplicationMailer
     @utm = "utm_source=TMC&utm_medium=welcome_new_user&" \
       "utm_campaign=transactional_emails"
     @user = User.find(user_id)
-    @results = Product.search('*', order: { times_sold: :desc }).take(4)
-    mail(to: @user.email,
-         subject: "#{I18n.t('emails.welcome')} #{I18n.t('site_name')}"
-        )
+    @results = Product.search('*', order: { times_sold: :desc }).first(4)
+    mail(to: @user.email, subject: "Welcome to The Montessori Company")
   end
 end
