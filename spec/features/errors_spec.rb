32520abd43fd8d5c872be20077f131d2152bf42b
Jan Klimo <jan.klimo@gmail.com>
2016-09-08 17:45:19 +0700

    make sure Airbrake works properly

--- spec/features/errors_spec.rb
+++ spec/features/errors_spec.rb
@@ -6,12 +6,22 @@ describe 'Error pages', type: :feature do
       expect(page).to have_content 'does not exist'
       expect(page).to have_link 'Keep shopping'
     end
+    it 'notifies Airbrake' do
+      allow(Airbrake).to receive(:notify)
+      visit order_path 1
+      expect(Airbrake).to have_received(:notify)
+    end
   end
   context 'page not found' do
     it 'redirects to 404' do
       visit '/made-up-path'
       expect(page).to have_content 'does not exist'
       expect(page).to have_link 'Keep shopping'
+    end
+    it 'does not notify Airbrake' do
+      allow(Airbrake).to receive(:notify)
+      visit '/made-up'
+      expect(Airbrake).not_to have_received(:notify)
     end
   end
   context '500' do
@@ -23,6 +33,11 @@ describe 'Error pages', type: :feature do
       visit order_path 1
       expect(page).to have_content 'highly trained monkeys'
       expect(page).to have_link "Let's go"
+    end
+    it 'notifies Airbrake' do
+      allow(Airbrake).to receive(:notify)
+      visit order_path 1
+      expect(Airbrake).to have_received(:notify)
     end
   end
 end
