48472ab65776cd640bd39cdc92d823228de46287
Jan Klimo <jan.klimo@gmail.com>
2016-10-27 11:12:39 +0700

    Add Free materials button within hero section

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,11 @@
+describe 'Home page', type: :feature do
+  describe 'free materials button' do
+    it 'redirects to the free materials page' do
+      visit root_path
+      within('#hero') do
+        click_link 'Download our free materials'
+      end
+      expect(page).to have_content 'Free Montessori Materials'
+    end
+  end
+end
