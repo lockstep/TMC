8093f913586f9910bff0647167d27a74160bbf86
Paul Hiatt <hiattp@gmail.com>
2016-11-19 16:42:27 +0700

    updated verbiage

--- app/helpers/socials_helper.rb
+++ app/helpers/socials_helper.rb
@@ -1,9 +1,8 @@
 module SocialsHelper
   DEFAULT_IMAGE = 'https://s3.amazonaws.com/lockstep-public-assets/tmc/tmc-primary-logo.png'
   DEFAULT_AUTHOR = 'The Montessori Company'
-  DEFAULT_DESCRIPTION = "Montessori materials lovingly hand-crafted by an " \
-    "AMI Montessori guide. Our Montessori materials are perfect for " \
-    "primary or home-school environments."
+  DEFAULT_DESCRIPTION = "Beautiful Montessori resources lovingly crafted " \
+    "& curated by an AMI Montessori guide."
   TWITTER_ACCOUNT = '@themontessorico'
   FB_PAGE = 'https://www.facebook.com/themontessoricompany/'
 end
