b99b0670228c52c25fa46d76afc2267fabf11a25
Paul Hiatt <hiattp@gmail.com>
2016-11-27 08:18:37 +0700

    added shipping address to new vendor purchase email

--- spec/mailers/orders_mailer_spec.rb
+++ spec/mailers/orders_mailer_spec.rb
@@ -7,8 +7,10 @@ describe OrdersMailer, type: :mailer do
   fixtures :products
 
   let(:michelle)      { users(:michelle) }
+  let(:mobile_order_completed) { orders(:mobile_order_completed) }
   let(:cards_order)   { orders(:cards_order) }
   let(:cards_charge)  { charges(:cards_charge) }
+  let(:line_item) { line_items(:mobile_order_lines) }
 
   describe '.confirmed_order' do
     before { OrdersMailer.confirmed_order(cards_order.id).deliver_now }
@@ -16,6 +18,14 @@ describe OrdersMailer, type: :mailer do
     it_behaves_like "sending_email" do
       let(:recipients) { [ michelle.email] }
       let(:subject)    { "Your order is ready!" }
+    end
+  end
+
+  describe '.notify_vendor' do
+    before { OrdersMailer.notify_vendor(line_item.id).deliver_now }
+
+    it "includes the recipients email" do
+      expect(ActionMailer::Base.deliveries.first.to_s).to match('3108 Bent')
     end
   end
 end
