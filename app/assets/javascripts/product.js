6e90c91333df95e979a0ccbd5c5b59abfef3100f
Paul Hiatt <hiattp@gmail.com>
2017-02-06 12:21:09 +0700

    disabled submit buttons when changing languages

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -4,6 +4,7 @@ document.addEventListener("turbolinks:load", function() {
   var changeLanguageFormSelector = '.select-language-form';
   $(changeLanguageFormSelector).find('.select-language-submission').hide();
   $(document).on('change', changeLanguageFormSelector + ' select', function(e) {
+    $(':input[type="submit"]').prop('disabled', true);
     $(changeLanguageFormSelector).closest('form').submit();
   });
 
