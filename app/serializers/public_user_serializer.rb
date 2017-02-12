50049e6ba8c2e1b58fe9f38702992f7dce42d87b
Paul Hiatt <hiattp@gmail.com>
2017-02-12 17:09:38 +0700

    added position with org to json

--- app/serializers/public_user_serializer.rb
+++ app/serializers/public_user_serializer.rb
@@ -1,7 +1,8 @@
 class PublicUserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :position,
     :organization_name, :address_country, :avatar_url_thumb,
-    :avatar_url_small, :avatar_url_medium, :full_address_country
+    :avatar_url_small, :avatar_url_medium, :full_address_country,
+    :position_with_organization
 
   def avatar_url_thumb
     object.avatar.url(:thumb)
@@ -13,5 +14,10 @@ class PublicUserSerializer < ActiveModel::Serializer
 
   def avatar_url_medium
     object.avatar.url(:medium)
+  end
+
+  def position_with_organization
+    return object.position if object.organization_name.blank?
+    "#{object.position} at #{object.organization_name}"
   end
 end
--- spec/controllers/directory_controller_spec.rb
+++ spec/controllers/directory_controller_spec.rb
@@ -9,7 +9,15 @@ describe DirectoryController, type: :request do
       profile = response_json['profiles'][0]
       expect(profile['first_name']).to eq 'Jane'
       expect(profile['full_address_country']).to eq 'Canada'
+      expect(profile['position_with_organization']).to eq User::POSITIONS.first
       expect(profile['email']).to be_nil
+    end
+    it 'returns full position with org when present' do
+      @user.update(organization_name: 'home')
+      get '/directory.json'
+      profile = response_json['profiles'][0]
+      expect(profile['position_with_organization'])
+        .to eq(User::POSITIONS.first + ' at home')
     end
   end
 end
