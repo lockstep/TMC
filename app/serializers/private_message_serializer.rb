fb3e58cf0e52bb8fb1fcb6a3e099fac0e6126fed
Sasitorn Aramcharoenrat <sasitorn.oeam@gmail.com>
2017-04-20 11:48:19 +0700

    Added created time ago into private message endpoint

--- app/serializers/private_message_serializer.rb
+++ app/serializers/private_message_serializer.rb
@@ -1,3 +1,9 @@
 class PrivateMessageSerializer < ActiveModel::Serializer
-  attributes :id, :message
+  attributes :id, :message, :created_time_ago
+
+  include ActionView::Helpers::DateHelper
+  
+  def created_time_ago
+    time_ago_in_words(object.created_at)
+  end
 end
