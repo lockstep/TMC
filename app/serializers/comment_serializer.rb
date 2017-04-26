f5292f124250f924ff9bf61d5fcdfce65233512d
Sasitorn Aramcharoenrat <sasitorn.oeam@gmail.com>
2017-04-26 14:14:32 +0700

    Added comment into breakout session endpoint

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -2,6 +2,7 @@ class CommentSerializer < ActiveModel::Serializer
   attributes :id, :message, :image_url_large
 
   def image_url_large
+    return unless object.image?
     object.image&.url(:large)
   end
 end
