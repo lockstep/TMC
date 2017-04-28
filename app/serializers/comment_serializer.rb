046bfc1b8920be08efedcdbba35d5d4cf80576ab
Sasitorn Aramcharoenrat <sasitorn.oeam@gmail.com>
2017-04-28 15:35:52 +0700

    Added user info into comment node

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,6 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :message, :image_url_large
+  has_one :author, serializer: PublicUserSerializer
 
   def image_url_large
     return unless object.image?
