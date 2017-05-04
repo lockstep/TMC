fe80a8722c0cf22bc56fa897abe423b5a6996e86
Paul Hiatt <hiattp@gmail.com>
2017-05-04 18:07:37 -0400

    do not retry the importer

--- app/workers/external_conference_registration_importer.rb
+++ app/workers/external_conference_registration_importer.rb
@@ -1,5 +1,6 @@
 class ExternalConferenceRegistrationImporter
   include Sidekiq::Worker
+  sidekiq_options retry: false
 
   def perform(import_id)
     ExternalConferenceRegistrationImport.find(import_id).import!
