1a007bbaba76654091461c0f64959c418481f4ac
birdytripler <birdy.tripler@gmail.com>
2017-05-16 11:53:50 +0700

    Add owner of image fields in conference API

--- app/serializers/image_serializer.rb
+++ app/serializers/image_serializer.rb
@@ -1,7 +1,24 @@
 class ImageSerializer < ActiveModel::Serializer
-  attributes :id, :image_url_large
+  attributes :id, :image_url_large, :owner,
+    :avatar_url_thumb, :avatar_url_small, :avatar_url_medium
 
   def image_url_large
     object.image&.url(:large)
+  end
+
+  def owner
+    object.author.full_name
+  end
+
+  def avatar_url_thumb
+    object.author.avatar.url(:thumb)
+  end
+
+  def avatar_url_small
+    object.author.avatar.url(:small)
+  end
+
+  def avatar_url_medium
+    object.author.avatar.url(:medium)
   end
 end
