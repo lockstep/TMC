6517eaa02b59b6ef5fab36b03c698c21bb060700
birdytripler <birdy.tripler@gmail.com>
2017-05-16 21:10:23 +0700

    Amend conference image API

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::ImagesController < Api::V1::BaseController
     conference = Conference.find(params[:conference_id])
     images = conference.images.order(:created_at).page(page_num).per(15)
     render json: images,
-      each_serializer: ImageSerializer, root: 'images',
+      each_serializer: CommentSerializer, root: 'images',
       meta: {
         current_page: page_num,
         per_page: 15,
@@ -23,7 +23,7 @@ class Api::V1::ImagesController < Api::V1::BaseController
     )
     if image.save
       FeedItemImageResizeWorker.perform_async(image.id)
-      render json: image, root: 'image', serializer: ImageSerializer
+      render json: image, root: 'image', serializer: CommentSerializer
     else
       render json: { meta: { errors: image.errors }},
         status: :unprocessable_entity
