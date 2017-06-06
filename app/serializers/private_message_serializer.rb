a9dc2eacc923caaf49c4974f8361b3ebf033a0f4
Paul Hiatt <hiattp@gmail.com>
2017-06-06 15:55:53 -0600

    added author to private message serializer

--- app/serializers/private_message_serializer.rb
+++ app/serializers/private_message_serializer.rb
@@ -1,7 +1,8 @@
 class PrivateMessageSerializer < ActiveModel::Serializer
   include ActionView::Helpers::DateHelper
-  
+
   attributes :id, :message, :created_at_time_ago_in_words
+  has_one :author, serializer: PublicUserSerializer
 
   def created_at_time_ago_in_words
     time_ago_in_words(object.created_at)
