b68414098adaf0fe8386e57c85f78e9271efb12c
Roonglit Chareonsupkul <roonglit@gmail.com>
2017-07-13 10:14:04 +0700

    TMC-144 allow mobile user to register himself with more info (#59)

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -2,6 +2,17 @@ module Api
   module V1
     class Users::RegistrationsController < DeviseTokenAuth::RegistrationsController
       protect_from_forgery with: :null_session
+      before_action :configure_sign_up_params
+
+      protected
+
+      def configure_sign_up_params
+        devise_parameter_sanitizer.for(:sign_up) << [
+          :first_name, :last_name, :position, :organization_name,
+          :address_city, :address_state, :address_country,
+          :opted_in_to_public_directory
+        ]
+      end
     end
   end
 end
