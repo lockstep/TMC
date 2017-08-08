d50f188779e1cb0aa4aa5d442265eb6b0a251cf9
Paul Hiatt <hiattp@gmail.com>
2017-08-08 06:49:55 -0600

    fixup! fixing bug

--- spec/controllers/directory_controller_spec.rb
+++ spec/controllers/directory_controller_spec.rb
@@ -2,8 +2,7 @@ describe DirectoryController, type: :request do
 
   describe '#index' do
     before do
-      @user = create(:user, opted_in_to_public_directory: true,
-                     address_country: 'ES')
+      @user = create(:user, opted_in_to_public_directory: true)
     end
     it 'returns users in requested format' do
       get '/directory.json'
@@ -22,11 +21,12 @@ describe DirectoryController, type: :request do
     end
     context 'countries are in a hash' do
       it 'handles the hash appropriately' do
-        other_user = create(:user, opted_in_to_public_directory: true)
+        other_user = create(:user, opted_in_to_public_directory: true,
+                           address_country: 'ES')
         get '/directory?utf8=%E2%9C%93&query&countries%5B0%5D=ES&commit=Search'
         users = assigns(:users)
-        expect(users).to include @user
-        expect(users).not_to include other_user
+        expect(users).to include other_user
+        expect(users).not_to include @user
       end
     end
   end
